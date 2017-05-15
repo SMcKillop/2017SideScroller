40ac5632674bb16ad5dc23bd4cdc8c0a64435ab9
Unknown <mckillos1@hawkmail.newpaltz.edu>
2017-05-15 12:03:00 -0400

    Silver pipe teleportation

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    public Transform WarpTarget
+    {
+        get
+        {
+            return warpTarget;
+        }
+
+        set
+        {
+            warpTarget = value;
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        Debug.Log("An object collided");
+        
+         other.gameObject.transform.position = WarpTarget.transform.position;
+        
+        
+        
+    }
+    
+}
